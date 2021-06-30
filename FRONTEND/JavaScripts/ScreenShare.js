4049733fff6424b2e254dfd68de738d2ddbefdfd
sathvikareddykadukuntla168 <sathvikareddykadukuntla168@gmail.com>
2021-06-30 03:08:48 -0400

    Screenshare toggle done

--- FRONTEND/JavaScripts/ScreenShare.js
+++ FRONTEND/JavaScripts/ScreenShare.js
@@ -73,3 +73,59 @@ function refreshMyLocalStream(stream, localAudioTrackChange = false) {
       localMediaStream.getVideoTracks()[0].enabled = false;
     }
   }
+/**
+ * Enable - disable screen sharing
+ */
+ function toggleScreenSharing() {
+    const constraints = {
+      video: true,
+    };
+  
+    let screenMediaPromise;
+  
+    if (!isScreenStreaming) {
+      // on screen sharing start
+      if (navigator.getDisplayMedia) {
+        // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getDisplayMedia
+        screenMediaPromise = navigator.getDisplayMedia(constraints);
+      } else if (navigator.mediaDevices.getDisplayMedia) {
+        screenMediaPromise = navigator.mediaDevices.getDisplayMedia(constraints);
+      } else {
+        // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
+        screenMediaPromise = navigator.mediaDevices.getUserMedia({
+          video: {
+            mediaSource: "screen",
+          },
+        });
+      }
+    } else {
+      // on screen sharing stop
+      const audioSource = audioInputSelect.value;
+      const videoSource = videoSelect.value;
+      const constraints = {
+        audio: { deviceId: audioSource ? { exact: audioSource } : undefined },
+        video: { deviceId: videoSource ? { exact: videoSource } : undefined },
+      };
+      screenMediaPromise = navigator.mediaDevices.getUserMedia(constraints);
+      // if screen sharing accidentally closed
+      if (isStreamRecording) {
+        stopStreamRecording();
+      }
+    }
+    screenMediaPromise
+      .then((screenStream) => {
+        // stop cam video track on screen share
+        stopLocalVideoTrack();
+        isScreenStreaming = !isScreenStreaming;
+        refreshMyStreamToPeers(screenStream);
+        refreshMyLocalStream(screenStream);
+        myVideo.classList.toggle("mirror");
+        setScreenSharingStatus(isScreenStreaming);
+        //setMyVideoStatusTrue();
+      })
+      .catch((err) => {
+        console.error("[Error] Unable to share the screen", err);
+        userLog("error", "Unable to share the screen " + err);
+      });
+  }
+  
