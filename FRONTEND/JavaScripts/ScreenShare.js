71ea83be29b8f8d1a89728c161da6179ee57d352
sathvikareddykadukuntla168 <sathvikareddykadukuntla168@gmail.com>
2021-07-01 21:38:05 -0400

    Screen share status updated

--- FRONTEND/JavaScripts/ScreenShare.js
+++ FRONTEND/JavaScripts/ScreenShare.js
@@ -128,4 +128,59 @@ function refreshMyLocalStream(stream, localAudioTrackChange = false) {
         userLog("error", "Unable to share the screen " + err);
       });
   }
-  
+ 
+  /**
+   * Set Screen Sharing Status
+   * @param {*} status
+   */
+   function setScreenSharingStatus(status) {
+    screenShareBtn.className = status ? "fas fa-stop-circle" : "fas fa-desktop";
+    // only for desktop
+    if (!isMobileDevice) {
+      tippy(screenShareBtn, {
+        content: status ? "STOP screen sharing" : "START screen sharing",
+        placement: "right-start",
+      });
+    }
+  }
+  function refreshMyLocalStream(stream, localAudioTrackChange = false) {
+  stream.getVideoTracks()[0].enabled = true;
+
+  // enable audio
+  if (localAudioTrackChange && myAudioStatus === false) {
+    audioBtn.className = "fas fa-microphone";
+    setMyAudioStatus(true);
+    myAudioStatus = true;
+  }
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/MediaStream
+  const newStream = new MediaStream([
+    stream.getVideoTracks()[0],
+    localAudioTrackChange
+      ? stream.getAudioTracks()[0]
+      : localMediaStream.getAudioTracks()[0],
+  ]);
+  localMediaStream = newStream;
+
+  // log newStream devices
+  console.log("refreshMyLocalStream", {
+    audio: localMediaStream.getAudioTracks()[0].label,
+    video: localMediaStream.getVideoTracks()[0].label,
+  });
+
+  // attachMediaStream is a part of the adapter.js library
+  attachMediaStream(myVideo, localMediaStream); // newstream
+
+  // on toggleScreenSharing video stop
+  stream.getVideoTracks()[0].onended = () => {
+    if (isScreenStreaming) toggleScreenSharing();
+  };
+
+  /** when you stop the screen sharing, on default i turn back to the webcam with video stream ON.
+   *  if you want the webcam with video stream OFF, just disable it with the button (click to video OFF),
+   *  before to stop the screen sharing.
+   */
+  if (myVideoStatus === false) {
+    localMediaStream.getVideoTracks()[0].enabled = false;
+  }
+}
